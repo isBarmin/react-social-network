7a7243dedaf12c2b5e9868da2fb846fae562842d
barmin <isbarmin@ya.ru>
2020-05-02 23:03:20 +0500

    добавил поддержку санок

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
 import reducers from './reducers';
 
 const store = createStore(
   combineReducers(reducers),
+  applyMiddleware(thunkMiddleware),
   // eslint-disable-next-line no-underscore-dangle
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
