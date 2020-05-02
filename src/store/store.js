1dea109c93e79e455590350bec8855b1fd55b6c1
barmin <isbarmin@ya.ru>
2020-05-02 17:46:14 +0500

    поддержка redux devtools

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,10 @@
 import { createStore, combineReducers } from 'redux';
 import reducers from './reducers';
 
-const store = createStore(combineReducers(reducers));
+const store = createStore(
+  combineReducers(reducers),
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
 
 export default store;
